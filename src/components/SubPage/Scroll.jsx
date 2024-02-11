d65754939b1268b43423fb46813ee6eca9aa24fa
Luis <141294557+LuisECV0@users.noreply.github.com>
2024-02-10 19:31:36 -0500

    implementacion de galeria de imagenes

--- src/components/SubPage/Scroll.jsx
+++ src/components/SubPage/Scroll.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import '../pages/Styles/Scroll.css';
+import uno from '../imgs/uno.jpg';
 
 export const Scroll = () => {
   return (
-    <div className="family-enjoyment-container">
-      <h2>¡Disfruta en Familia!</h2>
-      <p>Descubre momentos increíbles juntos.</p>
-      <button>Ver Más</button>
+    <div className="body-container">
+      <div className="scroll-box">
+        <p>DESCANSO Y CONFORT</p>
+        <h2>¡Disfruta en Familia!</h2>
+        <a>Ver Galería</a>
+      </div>
 
       <div className="image-scroll-container">
-        <img src="./images/1.jpg" alt="" />
+      <img src={uno} alt="" />
       </div>
     </div>
   );
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ViteReactSvgPlugin from 'vite-plugin-react-svg';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    ViteReactSvgPlugin({
+      defaultExport: 'component',
+      svgoConfig: {},
+    }),
+  ],
+});
