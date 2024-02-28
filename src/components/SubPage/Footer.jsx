8c47ee35e5fa9c4ec4b8a1c2c27dce644f1d75f4
Luis <141294557+LuisECV0@users.noreply.github.com>
2024-02-27 19:30:29 -0500

    correcion en el open navbar

--- src/components/SubPage/Footer.jsx
+++ src/components/SubPage/Footer.jsx
@@ -1,55 +1,53 @@
-// footer.jsx
+// Footer.jsx
 import React from "react";
 import '../pages/Styles/Footer.css';
-
+import '@fortawesome/fontawesome-free/css/all.css';
 
 const Footer = () => {
   return (
-
     <footer className="footer">
-    <div className="container">
+      <div className="container">
         <div className="row">
-            <div className="footer-col">
-                <h4>Hacienda</h4>
-                <ul>
-                    <li><a href="#">no se</a></li>
-                    <li><a href="#">no se</a></li>
-                    <li><a href="#">no se</a></li>
-                    <li><a href="#">no se</a></li>
-                </ul>
+          <div className="footer-col">
+            <h4>Hacienda</h4>
+            <ul>
+              <li><a href="#">no se</a></li>
+              <li><a href="#">no se</a></li>
+              <li><a href="#">no se</a></li>
+              <li><a href="#">no se</a></li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>soporte</h4>
+            <ul>
+              <li><a href="#">no se</a></li>
+              <li><a href="#">no se</a></li>
+              <li><a href="#">no se</a></li>
+              <li><a href="#">no se</a></li>
+              <li><a href="#">no se</a></li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>no se </h4>
+            <ul>
+              <li><a href="#">no se</a></li>
+              <li><a href="#">no se</a></li>
+              <li><a href="#">no se</a></li>
+              <li><a href="#">no se</a></li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>Redes Sociales</h4>
+            <div className="social-links">
+              <a href="#"><i className="fab fa-facebook"></i></a>
+              <a href="#"><i className="fab fa-tiktok"></i></a>
+              <a href="#"><i className="fab fa-instagram"></i></a>
+              <a href="#"><i className="fab fa-youtube"></i></a>
             </div>
-            <div className="footer-col">
-                <h4>soporte</h4>
-                <ul>
-                    <li><a href="#">no se</a></li>
-                    <li><a href="#">no se</a></li>
-                    <li><a href="#">no se</a></li>
-                    <li><a href="#">no se</a></li>
-                    <li><a href="#">no se</a></li>
-                </ul>
-            </div>
-            <div className="footer-col">
-                <h4>no se </h4>
-                <ul>
-                    <li><a href="#">no se</a></li>
-                    <li><a href="#">no se</a></li>
-                    <li><a href="#">no se</a></li>
-                    <li><a href="#">no se</a></li>
-                </ul>
-            </div>
-            <div className="footer-col">
-                <h4>Redes Sociales</h4>
-                <div className="social-links">
-                    <a href="#"><i className="fa-brands fa-facebook"></i></a>
-                    <a href="#"><i className="fa-brands fa-tiktok"></i></a>
-                    <a href="#"><i className="fab fa-instagram"></i></a>
-                    <a href="#"><i className="fa-brands fa-youtube"></i></a>
-                    
-                </div>
-            </div>
+          </div>
         </div>
-    </div>
-</footer>
+      </div>
+    </footer>
   );
 };
 
